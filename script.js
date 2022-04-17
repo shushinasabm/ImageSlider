959a3da417292b0a07461583b666edb76fefa9e8
shushinasabm <98877361+shushinasabm@users.noreply.github.com>
2022-04-17 21:34:42 +0430

    move pic

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
-let imageCenter =document.getElementsByClassName('photos-main')
-let textParagraph=document.getElementsByClassName('text-ph')
-let photoHeader =document.getElementsByClassName('photoHeader')
-let textHeader =document.getElementsByClassName('text-header')
-let leftButton=document.querySelector('.leftButton')
-let rightButton=document.querySelector('.rightButton')
+let getSlide = (slide) => {
+    let slide_logo = slide.querySelector('.image-logo').getAttribute('src')
+    let slide_title = slide.querySelector('.image-title').innerText
+    let slide_main = slide.querySelector('.image-main').getAttribute('src')
+    let slide_description = slide.querySelector('.image-description').innerText
 
-setInterval(function () {
-    imageCenter[0].src=imageCenter[1].src
-    imageCenter[1].src=imageCenter[2].src
-    imageCenter[2].src=imageCenter[0].src
-    photoHeader[0].src=photoHeader[1].src
-    photoHeader[1].src=photoHeader[2].src
-    photoHeader[2].src=photoHeader[0].src
-    textParagraph[0].innerText=textParagraph[1].innerText
-    textParagraph[1].innerText=textParagraph[2].innerText
-    textParagraph[2].innerText=textParagraph[0].innerText
-    textHeader[0].innerText=textHeader[1].innerText
-    textHeader[1].innerText=textHeader[2].innerText
-    textHeader[2].innerText=textHeader[0].innerText
-},2000)
+    return {
+        slideLogo: slide_logo,
+        slideTitle: slide_title,
+        slideMain: slide_main,
+        slideDescription: slide_description,
+    }
+}
+let setSlide = (lastSlide, mainSlide,firstSlide) => {
+    // let last_slide_logo = document.querySelector('#last_image_log').getAttribute('src')
+    let lastSlideInfo = getSlide(lastSlide)
+    let mainSlideInfo = getSlide(mainSlide)
+    let firstSlideInfo = getSlide(firstSlide)
+    mainSlide.querySelector('.image-logo').setAttribute('src', lastSlideInfo.slideLogo)
+    mainSlide.querySelector('.image-title').innerText = lastSlideInfo.slideTitle
+    mainSlide.querySelector('.image-main').setAttribute('src', lastSlideInfo.slideMain)
+    mainSlide.querySelector('.image-description').innerText = lastSlideInfo.slideDescription
+    lastSlide.querySelector('.image-logo').setAttribute('src', firstSlideInfo.slideLogo)
+    lastSlide.querySelector('.image-title').innerText = firstSlideInfo.slideTitle
+    lastSlide.querySelector('.image-main').setAttribute('src', firstSlideInfo.slideMain)
+    lastSlide.querySelector('.image-description').innerText = firstSlideInfo.slideDescription
+    firstSlide.querySelector('.image-logo').setAttribute('src', mainSlideInfo.slideLogo)
+    firstSlide.querySelector('.image-title').innerText = mainSlideInfo.slideTitle
+    firstSlide.querySelector('.image-main').setAttribute('src', mainSlideInfo.slideMain)
+    firstSlide.querySelector('.image-description').innerText = mainSlideInfo.slideDescription
+}
+setInterval( () => {
+    let left_image = document.querySelector('.image-left')
+    let main_image = document.querySelector('.image-center')
+    let right_image = document.querySelector('.image-right')
+    setSlide(left_image, main_image,right_image)
+}, 2000)
 
-leftButton.addEventListener('click',function (event) {
-        imageCenter[0].src=imageCenter[1].src
-    imageCenter[1].src=imageCenter[2].src
-    imageCenter[2].src=imageCenter[0].src
-    photoHeader[0].src=photoHeader[1].src
-    photoHeader[1].src=photoHeader[2].src
-    photoHeader[2].src=photoHeader[0].src
-    textParagraph[0].innerText=textParagraph[1].innerText
-    textParagraph[1].innerText=textParagraph[2].innerText
-    textParagraph[2].innerText=textParagraph[0].innerText
-    textHeader[0].innerText=textHeader[1].innerText
-    textHeader[1].innerText=textHeader[2].innerText
-    textHeader[2].innerText=textHeader[0].innerText
-})
-rightButton.addEventListener('click',function (event) {
-    imageCenter[1].src=imageCenter[0].src
-    imageCenter[0].src=imageCenter[2].src
-    imageCenter[2].src=imageCenter[1].src
-    photoHeader[1].src=photoHeader[0].src
-    photoHeader[0].src=photoHeader[2].src
-    photoHeader[2].src=photoHeader[1].src
-    textParagraph[1].innerText=textParagraph[0].innerText
-    textParagraph[0].innerText=textParagraph[2].innerText
-    textParagraph[2].innerText=textParagraph[1].innerText
-    textHeader[1].innerText=textHeader[0].innerText
-    textHeader[0].innerText=textHeader[2].innerText
-    textHeader[2].innerText=textHeader[1].innerText
-})
