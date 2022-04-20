7f8e413296b95b602d586b066fb18a6a2224935e
shushinasabm <98877361+shushinasabm@users.noreply.github.com>
2022-04-20 21:24:41 +0430

    create code button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let rightBbutton = document.querySelector('.rightButton')
+let leftBbutton = document.querySelector('.leftButton')
+let sliders = document.querySelector('.image-center')
 let getSlide = (slide) => {
     let slide_logo = slide.querySelector('.image-logo').getAttribute('src')
     let slide_title = slide.querySelector('.image-title').innerText
     let slide_main = slide.querySelector('.image-main').getAttribute('src')
     let slide_description = slide.querySelector('.image-description').innerText
+
 
     return {
         slideLogo: slide_logo,
@@ -11,8 +15,7 @@ let getSlide = (slide) => {
         slideDescription: slide_description,
     }
 }
-let setSlide = (lastSlide, mainSlide,firstSlide) => {
-    // let last_slide_logo = document.querySelector('#last_image_log').getAttribute('src')
+let setSlide = (lastSlide, mainSlide, firstSlide) => {
     let lastSlideInfo = getSlide(lastSlide)
     let mainSlideInfo = getSlide(mainSlide)
     let firstSlideInfo = getSlide(firstSlide)
@@ -29,10 +32,34 @@ let setSlide = (lastSlide, mainSlide,firstSlide) => {
     firstSlide.querySelector('.image-main').setAttribute('src', mainSlideInfo.slideMain)
     firstSlide.querySelector('.image-description').innerText = mainSlideInfo.slideDescription
 }
-setInterval( () => {
+
+let sliderInterval = setInterval(() => {
     let left_image = document.querySelector('.image-left')
     let main_image = document.querySelector('.image-center')
     let right_image = document.querySelector('.image-right')
-    setSlide(left_image, main_image,right_image)
+    setSlide(left_image, main_image, right_image)
 }, 2000)
 
+sliders.addEventListener('mouseover', function (event) {
+    clearInterval(sliderInterval)
+})
+sliders.addEventListener('mouseout', function (event) {
+    sliderInterval = setInterval(() => {
+        let left_image = document.querySelector('.image-left')
+        let main_image = document.querySelector('.image-center')
+        let right_image = document.querySelector('.image-right')
+        setSlide(left_image, main_image, right_image)
+    }, 2000)
+})
+rightBbutton.addEventListener('click', function (event) {
+    let left_image = document.querySelector('.image-left')
+    let main_image = document.querySelector('.image-center')
+    let right_image = document.querySelector('.image-right')
+    setSlide(left_image, main_image, right_image)
+})
+leftBbutton.addEventListener('click', function (event) {
+    let left_image = document.querySelector('.image-left')
+    let main_image = document.querySelector('.image-center')
+    let right_image = document.querySelector('.image-right')
+    setSlide(right_image, main_image, left_image)
+})
